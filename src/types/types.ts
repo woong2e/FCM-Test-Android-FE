0a03f29c701ec7857251d2828faf3a5a4b25a0aa
woong2e <tlswodnd12345@naver.com>
2024-03-20 17:49:36 +0900

    타입 정의

---
+++ src/types/types.ts
@@ -0,0 +1,18 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import store from "../store";
+
+export type RootStackParamList = {
+    SignUp: undefined;
+    SignIn: undefined;
+    Welcome: undefined;
+    Home: undefined;
+    Login: undefined;
+};
+
+export type SignUpProps = NativeStackScreenProps<RootStackParamList, "SignUp">;
+export type SignInProps = NativeStackScreenProps<RootStackParamList, "SignIn">;
+export type WelcomeProps = NativeStackScreenProps<RootStackParamList, "Welcome">;
+export type HomeProps = NativeStackScreenProps<RootStackParamList, "Home">;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
