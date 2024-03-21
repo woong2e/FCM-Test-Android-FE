19da8e594c8b88d18cb9aa9739ae609e04a0e0e6
woong2e <tlswodnd12345@naver.com>
2024-03-21 18:49:12 +0900

    . . .

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,36 +1,27 @@
 import React from 'react';
-import { Button, Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import { Button, Text, View, TouchableOpacity, StyleSheet, PermissionsAndroid } from "react-native";
 import { HomeProps } from '../types/types';
 import Styles from '../styles/Styles';
 import { useEffect } from 'react';
 import messaging from '@react-native-firebase/messaging';
+import notifee, { Notification, NotificationAndroid } from '@notifee/react-native';
 
 
 function Home({ navigation } : HomeProps) {
   useEffect(() => {
 		getFcmToken();
-		subscribe();
 		return () => {
-			subscribe()
 		};
 	}, []);
 
   const getFcmToken = async () => {
+    await messaging().registerDeviceForRemoteMessages();
 		const fcmToken = await messaging().getToken();
 		console.log('[+] FCM Token :: ', fcmToken);
 	};
 
-  const subscribe = messaging().onMessage(async remoteMessage => {
-		console.log('[+] Remote Message ', JSON.stringify(remoteMessage));
-	});
-
     return <View style={Styles.container}>
         <Text style={Styles.title}>Home</Text>
-        {/* <TouchableOpacity
-          style={Styles.button}
-          onPress={() => navigation.navigate('SignIn')}>
-          <Text style={Styles.buttonText}>Sign In</Text>
-        </TouchableOpacity> */}
         </View>
 }
 
