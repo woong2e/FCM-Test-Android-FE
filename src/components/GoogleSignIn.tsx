4c588309114094e48984e78c8a6832b79d4bf72c
woong2e <tlswodnd12345@naver.com>
2024-03-26 14:50:00 +0900

    feat: 구글 로그인기능

--- src/components/GoogleSignIn.tsx
+++ src/components/GoogleSignIn.tsx
@@ -2,11 +2,33 @@ import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/go
 import auth from '@react-native-firebase/auth'
 import Styles from '../styles/Styles';
 import { View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../hooks';
+import { setLoggedIn } from '../store/loginSuccessSlice';
 
 function GoogleSignIn() {
+  const dispatch = useAppDispatch();    
+  const isLogin = useAppSelector((state) => state.isLogin);
+  const checkLoggedIn = () => {
+    auth().onAuthStateChanged((user) => {
+        if (user) {
+          dispatch(setLoggedIn(true));
+        } else {
+          dispatch(setLoggedIn(false));
+        }
+        console.log(isLogin.isLogin)
+    })
+  }
+
+  useEffect(() => {
+    checkLoggedIn();
+  }, [])
+
   const onGoogleButtonPress = async () => {
+    console.log("됐나?");
     // Get the users ID token
     const { idToken } = await GoogleSignin.signIn();
+    console.log(idToken);
     // Create a Google credential with the token
     const googleCredential = auth.GoogleAuthProvider.credential(idToken);
     // Sign-in the user with the credential
@@ -14,9 +36,9 @@ function GoogleSignIn() {
   }
 
   return (
-    <View>
-      <GoogleSigninButton onPress={() => onGoogleButtonPress()} />
-    </View>
+      <GoogleSigninButton 
+      onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!')).catch(error => {console.log(error)})
+}  />
   );
 }
 
